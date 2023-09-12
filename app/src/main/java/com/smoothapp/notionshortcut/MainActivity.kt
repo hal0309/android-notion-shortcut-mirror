85715fadd512f787c7c8eccfef5ae33c4f18731f
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2023-09-12 22:39:03 +0900

    Initial commit

---
+++ app/src/main/java/com/smoothapp/notionshortcut/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.smoothapp.notionshortcut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
