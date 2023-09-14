3fbe13ce1fcb3305e21ae7482e850f85f53c09a8
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2023-09-14 14:12:58 +0900

    feature shortcut

--- app/src/main/java/com/smoothapp/notionshortcut/MainActivity.kt
+++ app/src/main/java/com/smoothapp/notionshortcut/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.smoothapp.notionshortcut
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
 import com.smoothapp.notionshortcut.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.apply {
+
+            root.setOnClickListener {
+                val intent = Intent(Intent.ACTION_MAIN)
+
+                val shortcut = ShortcutInfoCompat.Builder(this@MainActivity, "test2")
+                    .setShortLabel("test2")
+                    .setLongLabel("test2_longtext")
+                    .setIntent(intent)
+                    .build()
+                ShortcutManagerCompat.pushDynamicShortcut(this@MainActivity, shortcut)
+            }
 
         }
     }
