94805732c184030b973fa27c5824f4834e5a73ab
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2023-10-11 18:11:47 +0900

    20231011 date fragment listener WIP

--- app/src/main/java/com/smoothapp/notionshortcut/view/fragment/NotionDateFragment.kt
+++ app/src/main/java/com/smoothapp/notionshortcut/view/fragment/NotionDateFragment.kt
@@ -17,18 +17,13 @@ import com.smoothapp.notionshortcut.databinding.FragmentNotionStatusBinding
 import com.smoothapp.notionshortcut.model.constant.NotionColorEnum
 import com.smoothapp.notionshortcut.model.entity.NotionPostTemplate
 import com.smoothapp.notionshortcut.view.adapter.NotionSelectListAdapter
+import java.util.Date
 
 class NotionDateFragment : Fragment() {
 
 
     private lateinit var binding: FragmentNotionDateBinding
     private var listener: Listener? = null
-
-    private lateinit var startDatePicker: MaterialDatePicker<Long>
-    private lateinit var startTimePicker: MaterialTimePicker
-    private lateinit var endDatePicker: MaterialDatePicker<Long>
-    private lateinit var endTimePicker: MaterialTimePicker
-
 
     private var isViewCreated = false
     private var isListInitialized = false
@@ -41,22 +36,29 @@ class NotionDateFragment : Fragment() {
         binding = FragmentNotionDateBinding.inflate(inflater, container, false)
         binding.apply {
             val commonPickerListener = createCommonPickerListener()
-            startDatePicker = MaterialComponentUtil.createDatePicker(listener = commonPickerListener)
-            startTimePicker = MaterialComponentUtil.createTimePicker(requireContext(), listener = commonPickerListener)
-            endDatePicker = MaterialComponentUtil.createDatePicker(listener = commonPickerListener)
-            endTimePicker = MaterialComponentUtil.createTimePicker(requireContext(), listener = commonPickerListener)
+
 
             startDateContainer.setOnClickListener {
                 if(!isPickerShowing){
                     isPickerShowing = true
-                    startDatePicker.show(parentFragmentManager, null)
+                    val picker = MaterialComponentUtil.createDatePicker(listener = commonPickerListener).apply {
+                        addOnPositiveButtonClickListener {
+                            Log.d("", Date(it).toString())
+                        }
+                    }
+                    picker.show(parentFragmentManager, null)
                 }
             }
 
             startTimeContainer.setOnClickListener {
                 if(!isPickerShowing){
                     isPickerShowing = true
-                    startTimePicker.show(parentFragmentManager, null)
+                    val picker = MaterialComponentUtil.createTimePicker(requireContext(), listener = commonPickerListener).apply {
+                        addOnPositiveButtonClickListener {
+                            Log.d("", "$hour $minute")
+                        }
+                    }
+                    picker.show(parentFragmentManager, null)
                 }
             }
 
