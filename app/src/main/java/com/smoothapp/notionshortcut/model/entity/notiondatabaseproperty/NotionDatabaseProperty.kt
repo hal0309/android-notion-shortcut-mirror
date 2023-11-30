1173a142a609f2ee5416890606f2f7b99efcf3fa
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2023-11-30 17:51:44 +0900

    20231130 edit README, refactor comment

--- app/src/main/java/com/smoothapp/notionshortcut/model/entity/notiondatabaseproperty/NotionDatabaseProperty.kt
+++ app/src/main/java/com/smoothapp/notionshortcut/model/entity/notiondatabaseproperty/NotionDatabaseProperty.kt
@@ -2,6 +2,12 @@ package com.smoothapp.notionshortcut.model.entity.notiondatabaseproperty
 
 import com.smoothapp.notionshortcut.model.constant.NotionApiPropertyEnum
 
+/**
+ * Notionデータベースのプロパティを表すクラス。
+ * @param type プロパティのタイプ(ex. checkbox, title, etc.)
+ * @param name プロパティ名
+ * @param contents プロパティの内容
+ */
 open class NotionDatabaseProperty(
     private val type: NotionApiPropertyEnum,
     private val name: String,
